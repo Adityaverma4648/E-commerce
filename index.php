7aa198c3a6dd548eb5b6ddae28ca040bba8fcb9e
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-01-10 02:03:59 +0530

    fetching and display categories

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--  the title -->
+    <title>E-commerce</title>
+    <!--  google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&family=Raleway&family=Roboto&display=swap" rel="stylesheet">
+    <!-- bootstrap5  -->
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- mystyles -->
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+
+<body>
+    <div class="categoriesCont container-fluid px-2 text-white d-flex justify-content-center align-items-center" id="categoriesCont">
+        <!--  fetched via index.js<fetchCategories -->
+    </div>
+    <div class="choosenCategoryCont d-flex " id="choosenCategoryCont"></div>
+
+
+
+    <script src="index.js"></script>
+</body>
+
+</html>
